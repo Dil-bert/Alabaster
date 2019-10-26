28edbfe22fe07daa443ed41a5089e6fb08725ed9
Dillon Archibald <archid1@unlv.nevada.edu>
2019-10-25 21:19:10 -0700

    DA3B Word doc Pre-Schematic and flowchart

--- DesignAssignments/DA3b/DA3B/DA3B/main.c
+++ DesignAssignments/DA3b/DA3B/DA3B/main.c
@@ -12,16 +12,13 @@
 #include <avr/interrupt.h>
 #define USART_BAUDRATE 9600
 #define UBRR_VALUE (((F_CPU/(USART_BAUDRATE*16UL)))-1)
-#define ADCINDEX 20
-//store ADC values
-//uint8_t wave[ADCINDEX];
 
-volatile uint8_t ii = 0;
+// Global Variables 
 volatile uint8_t tempL = 0;
 volatile uint8_t tempH = 0;
 volatile float tempOut = 0;
-//volatile uint8_t flag = 0;
 
+// Function definitions
 void InitPort();
 
 void InitADC();
@@ -40,6 +37,9 @@ ISR(ADC_vect);
 // Set Stream Pointer
 FILE usart0_str = FDEV_SETUP_STREAM(USART0SendByte, NULL, _FDEV_SETUP_WRITE);
 
+//-----------------------------------------------------------------------------
+//	**********MAIN**********
+//-----------------------------------------------------------------------------
 int main(void){
 	// Initialize USART0
 	USART0Init();
@@ -67,6 +67,9 @@ int main(void){
     }
 }
 
+//-----------------------------------------------------------------------------
+//	*********Port Initialization function*********
+//-----------------------------------------------------------------------------
 void InitPort(){
 	// Set pin C5 as an input pin
 	DDRC |= (0<<PINC5);
@@ -76,6 +79,9 @@ void InitPort(){
 	DIDR0 |= (1<<ADC5D);
 }
 
+//-----------------------------------------------------------------------------
+//	*********ADC Initialization function*********
+//-----------------------------------------------------------------------------
 void InitADC(){
 	// Select Vref=Avcc and set (left = ADLAR = 1)(right = ADLAR = 0) justified result
 	ADMUX |=(1<<REFS0)|(0<<ADLAR);
@@ -86,19 +92,31 @@ void InitADC(){
 	ADCSRB |=(1<<ADTS1)|(1<<ADTS0);
 }
 
+//-----------------------------------------------------------------------------
+//	*********ADC PORT SELECT function*********
+//-----------------------------------------------------------------------------
 void SetADCChannel(uint8_t ADCchannel){
 	// Select ADC channel with safety mask
 	ADMUX = (ADMUX & 0xF0)|(ADCchannel & 0x0F);
 }
 
+//-----------------------------------------------------------------------------
+//	*********ADC start function*********
+//-----------------------------------------------------------------------------
 void StartADC(void){
 	ADCSRA |=(1<<ADSC);
 }
 
+//-----------------------------------------------------------------------------
+//	*********ADC end function*********
+//-----------------------------------------------------------------------------
 void DisableADC(void){
 	ADCSRA &= ~((1<<ADEN)|(1<<ADIE));
 }
 
+//-----------------------------------------------------------------------------
+//	*********USART Initialization function*********
+//-----------------------------------------------------------------------------
 void USART0Init(void){
 	// Set baud rate
 	UBRR0H = (uint8_t)(UBRR_VALUE>>8);
@@ -109,6 +127,9 @@ void USART0Init(void){
 	UCSR0B |= (1<<RXEN0) | (1<<TXEN0);
 }
 
+//-----------------------------------------------------------------------------
+//	*********Timer0 Initialization function*********
+//-----------------------------------------------------------------------------
 void InitTimer0(void){
 	// Set Initial Timer value
 	TCNT0 = 0;
@@ -119,6 +140,9 @@ void InitTimer0(void){
 	TCCR0A |= (1<<COM0A0)|(1<<WGM01);
 }
 
+//-----------------------------------------------------------------------------
+//	*********USART Send function*********
+//-----------------------------------------------------------------------------
 int USART0SendByte(char u8Data,FILE *stream){
 	// Wait while previous byte is completed
 	while(!(UCSR0A & (1<<UDRE0))){};
@@ -127,17 +151,25 @@ int USART0SendByte(char u8Data,FILE *stream){
 	return 0;
 }
 
+//-----------------------------------------------------------------------------
+//	*********Timer start function*********
+//-----------------------------------------------------------------------------
 void StartTimer0(void){
 	// Set prescaller 8 and start timer
 	TCCR0B |= (1<<CS01);
 }
 
+//-----------------------------------------------------------------------------
+//	*********Timer end function*********
+//-----------------------------------------------------------------------------
 void StopTimer(void){
 	TCCR0B &= ~(1<<CS01);
 	TIMSK0 &= ~(1<<OCIE0A);
 }
 
-//ADC conversion complete ISR
+//-----------------------------------------------------------------------------
+//	*********ADC conversion complete ISR*********
+//-----------------------------------------------------------------------------
 ISR(ADC_vect){
 	// clear timer compare match flag
 	TIFR0 = (1<<OCF0A);
@@ -145,9 +177,4 @@ ISR(ADC_vect){
 	PIND = (1<<PD2);
 	tempL = ADCL;
 	tempH = ADCH;
-	//if(ii == ADCINDEX){ // read 20 values?
-	//	StopTimer();
-	//	DisableADC();
-	//	flag = 1;
-	//}
 }
